fa529beb1e332260b058bf39a6d7490554a55b02
Luka Wanyama <lukewanyama5@gmail.com>
2023-12-10 09:00:25 +0300

    Adding js to the website

---
+++ index.js
@@ -0,0 +1,8 @@
+const navigationMenu = 
+document.getElementById('nav-menu');
+
+const h2 = document.createElement("h2");
+
+h2.textContent = "This content added by Javascript";
+
+document.querySelector("body").appendChild(h2);
