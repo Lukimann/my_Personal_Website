3a330d3af59176224f96de9f5ed5d6e186d071ed
Luka Wanyama <lukewanyama5@gmail.com>
2023-12-10 09:25:47 +0300

    listeners on the js

--- index.js
+++ index.js
@@ -6,3 +6,8 @@ const h2 = document.createElement("h2");
 h2.textContent = "...";
 
 document.querySelector("body").appendChild(h2);
+
+const input = document.getElementById('input');
+input.addEventListener('click', function() {
+  alert('I was clicked!');
+});
